f9b453e6871fdeb1f41827170927d7ff7a7e7b39
Sulthonhanan <hananakhnaf31@gmail.com>
2025-12-25 19:44:23 +0700

    Penambahan dan penyesuaian kode Repository

--- app/src/main/java/com/example/prak12/repositori/RepositoriDataSiswa.kt
+++ app/src/main/java/com/example/prak12/repositori/RepositoriDataSiswa.kt
@@ -7,14 +7,28 @@ import retrofit2.Response
 interface RepositoriDataSiswa {
     suspend fun getDataSiswa(): List<DataSiswa>
     suspend fun postDataSiswa(dataSiswa: DataSiswa): Response<Void>
+    suspend fun getSatuSiswa(id: Int): DataSiswa
+    suspend fun editSatuSiswa(id: Int, dataSiswa: DataSiswa): Response<Void>
+    suspend fun hapusSatuSiswa(id: Int): Response<Void>
 }
 
 class JaringanRepositoriDataSiswa(
     private val serviceApiSiswa: ServiceApiSiswa
 ) : RepositoriDataSiswa {
+    // Memperbaiki nama fungsi dari getSiswa ke getDataSiswa
     override suspend fun getDataSiswa(): List<DataSiswa> =
-        serviceApiSiswa.getDataSiswa() // Menyesuaikan nama fungsi di ServiceApiSiswa
+        serviceApiSiswa.getDataSiswa()
 
+    // Memperbaiki nama fungsi dari postSiswa ke postDataSiswa
     override suspend fun postDataSiswa(dataSiswa: DataSiswa): Response<Void> =
-        serviceApiSiswa.postDataSiswa(dataSiswa) // Menyesuaikan nama fungsi di ServiceApiSiswa
+        serviceApiSiswa.postDataSiswa(dataSiswa)
+
+    override suspend fun getSatuSiswa(id: Int): DataSiswa =
+        serviceApiSiswa.getSatuSiswa(id)
+
+    override suspend fun editSatuSiswa(id: Int, dataSiswa: DataSiswa): Response<Void> =
+        serviceApiSiswa.editSatuSiswa(id, dataSiswa)
+
+    override suspend fun hapusSatuSiswa(id: Int): Response<Void> =
+        serviceApiSiswa.hapusSatuSiswa(id)
 }
