e1e68d3916c9646efa8975d1acaec022ade6a401
Sulthonhanan <hananakhnaf31@gmail.com>
2025-12-25 19:48:34 +0700

    Penambahan dan penyesuaian kode EntrySiswaScreen.kt.kt

--- app/src/main/java/com/example/prak12/view/EntrySiswaScreen.kt
+++ app/src/main/java/com/example/prak12/view/EntrySiswaScreen.kt
@@ -3,10 +3,49 @@ package com.example.prak12.view
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.prak12.R
+import com.example.prak12.uicontroller.route.DestinasiEntry
+import com.example.prak12.viewmodel.EntryViewModel
 import com.example.prak12.viewmodel.SiswaEvent
 import com.example.prak12.viewmodel.SiswaUIState
+import com.example.prak12.viewmodel.provider.PenyediaViewModel
+import kotlinx.coroutines.launch
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun EntrySiswaScreen(
+    navigateBack: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: EntryViewModel = viewModel(factory = PenyediaViewModel.Factory)
+) {
+    val coroutineScope = rememberCoroutineScope()
+    Scaffold(
+        topBar = {
+            SiswaTopAppBar(
+                title = stringResource(DestinasiEntry.titleRes),
+                canNavigateBack = true,
+                navigateUp = navigateBack
+            )
+        }
+    ) { innerPadding ->
+        EntryBody(
+            siswaUIState = viewModel.uiStateSiswa,
+            onSiswaValueChange = viewModel::updateUiState,
+            onSaveClick = {
+                coroutineScope.launch {
+                    viewModel.insertSiswa()
+                    navigateBack()
+                }
+            },
+            modifier = Modifier.padding(innerPadding)
+        )
+    }
+}
 
 @Composable
 fun EntryBody(
@@ -29,7 +68,7 @@ fun EntryBody(
             shape = MaterialTheme.shapes.small,
             modifier = Modifier.fillMaxWidth()
         ) {
-            Text(text = "Simpan")
+            Text(text = stringResource(R.string.btn_submit))
         }
     }
 }
@@ -49,7 +88,7 @@ fun FormInput(
         OutlinedTextField(
             value = siswaEvent.nama,
             onValueChange = { onValueChange(siswaEvent.copy(nama = it)) },
-            label = { Text("Nama") },
+            label = { Text(stringResource(R.string.nama)) },
             modifier = Modifier.fillMaxWidth(),
             enabled = enabled,
             singleLine = true
@@ -57,7 +96,7 @@ fun FormInput(
         OutlinedTextField(
             value = siswaEvent.alamat,
             onValueChange = { onValueChange(siswaEvent.copy(alamat = it)) },
-            label = { Text("Alamat") },
+            label = { Text(stringResource(R.string.alamat)) },
             modifier = Modifier.fillMaxWidth(),
             enabled = enabled,
             singleLine = true
@@ -65,7 +104,7 @@ fun FormInput(
         OutlinedTextField(
             value = siswaEvent.telpon,
             onValueChange = { onValueChange(siswaEvent.copy(telpon = it)) },
-            label = { Text("Telepon") },
+            label = { Text(stringResource(R.string.telpon)) },
             modifier = Modifier.fillMaxWidth(),
             enabled = enabled,
             singleLine = true
