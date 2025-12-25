57dda1be0bfb190be935e30ae25c4f6a2766b673
Sulthonhanan <hananakhnaf31@gmail.com>
2025-12-25 19:43:52 +0700

    Penambahan dan penyesuaian kode PenyediaViewModel

--- app/src/main/java/com/example/prak12/viewmodel/provider/PenyediaViewModel.kt
+++ app/src/main/java/com/example/prak12/viewmodel/provider/PenyediaViewModel.kt
@@ -1,10 +1,13 @@
 package com.example.prak12.viewmodel.provider
 
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.prak12.AplikasiSiswa
+import com.example.prak12.viewmodel.DetailViewModel
+import com.example.prak12.viewmodel.EditViewModel
 import com.example.prak12.viewmodel.EntryViewModel
 import com.example.prak12.viewmodel.HomeViewModel
 
@@ -19,6 +22,18 @@ object PenyediaViewModel {
         }
         initializer {
             EntryViewModel(aplikasiSiswa().container.repositoriDataSiswa)
+        }
+        initializer {
+            DetailViewModel(
+                this.createSavedStateHandle(),
+                aplikasiSiswa().container.repositoriDataSiswa
+            )
+        }
+        initializer {
+            EditViewModel(
+                this.createSavedStateHandle(),
+                aplikasiSiswa().container.repositoriDataSiswa
+            )
         }
     }
 }
