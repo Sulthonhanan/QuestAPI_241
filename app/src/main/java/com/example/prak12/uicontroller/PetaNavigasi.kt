40a95c9d63c3783c53b1d889ea3d6a414090c2d2
Sulthonhanan <hananakhnaf31@gmail.com>
2025-12-25 19:44:09 +0700

    Penambahan dan penyesuaian kode PetaNavigasi.kt

--- app/src/main/java/com/example/prak12/uicontroller/PetaNavigasi.kt
+++ app/src/main/java/com/example/prak12/uicontroller/PetaNavigasi.kt
@@ -3,21 +3,12 @@ package com.example.prak12.uicontroller
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
-import com.example.prak12.uicontroller.route.DestinasiEntry
-import com.example.prak12.uicontroller.route.DestinasiHome
-import com.example.prak12.view.EntrySiswaScreen
-import com.example.prak12.view.HomeScreen
-
-@Composable
-fun DataSiswaApp(
-    navController: NavHostController = rememberNavController(),
-    modifier: Modifier = Modifier
-) {
-    HostNavigasi(navController = navController, modifier = modifier)
-}
+import androidx.navigation.navArgument
+import com.example.prak12.uicontroller.route.*
+import com.example.prak12.view.*
 
 @Composable
 fun HostNavigasi(
@@ -31,16 +22,40 @@ fun HostNavigasi(
     ) {
         composable(DestinasiHome.route) {
             HomeScreen(
-                navigateToItemEntry = {
-                    navController.navigate(DestinasiEntry.route)
+                navigateToItemEntry = { navController.navigate(DestinasiEntry.route) },
+                onDetailClick = { id ->
+                    navController.navigate("${DestinasiDetail.route}/$id")
                 }
             )
         }
+
         composable(DestinasiEntry.route) {
-            EntrySiswaScreen(
-                navigateBack = {
-                    navController.navigateUp()
+            EntrySiswaScreen(navigateBack = { navController.popBackStack() })
+        }
+
+        composable(
+            route = DestinasiDetail.routeWithArgs,
+            arguments = listOf(navArgument(DestinasiDetail.ITEM_ID_ARG) { // Pakai Kapital
+                type = NavType.IntType
+            })
+        ) {
+            DetailSiswaScreen(
+                navigateBack = { navController.popBackStack() },
+                navigateToEditItem = { id ->
+                    navController.navigate("${DestinasiEdit.route}/$id")
                 }
+            )
+        }
+
+        composable(
+            route = DestinasiEdit.routeWithArgs,
+            arguments = listOf(navArgument(DestinasiEdit.ITEM_ID_ARG) { //
+                type = NavType.IntType
+            })
+        ) {
+            EditSiswaScreen(
+                navigateBack = { navController.popBackStack() },
+                onNavigateUp = { navController.navigateUp() }
             )
         }
     }
