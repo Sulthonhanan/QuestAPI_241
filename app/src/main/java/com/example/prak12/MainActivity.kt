ceb7cd4df7b74cebd9a769151b1293d7f26c4b1a
Sulthonhanan <hananakhnaf31@gmail.com>
2025-12-25 19:43:33 +0700

    Penambahan dan penyesuaian kode MainActivity

--- app/src/main/java/com/example/prak12/MainActivity.kt
+++ app/src/main/java/com/example/prak12/MainActivity.kt
@@ -7,9 +7,11 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.navigation.compose.rememberNavController
 import com.example.prak12.ui.theme.Prak12Theme
-import com.example.prak12.uicontroller.DataSiswaApp
+import com.example.prak12.uicontroller.HostNavigasi
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,13 +19,21 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Prak12Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-
-                    DataSiswaApp(
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                // Panggil fungsi pembungkus utama
+                DataSiswaApp()
             }
         }
+    }
+}
+
+@Composable
+fun DataSiswaApp(modifier: Modifier = Modifier) {
+    val navController = rememberNavController() // Menginisialisasi controller navigasi
+    Scaffold(modifier = modifier.fillMaxSize()) { innerPadding ->
+        // Memanggil HostNavigasi yang berisi rute Home, Detail, dan Edit
+        HostNavigasi(
+            navController = navController,
+            modifier = Modifier.padding(innerPadding)
+        )
     }
 }
