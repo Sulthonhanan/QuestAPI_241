897c6f2e3ba6c7615cc30f60965b4445addf9039
Sulthonhanan <hananakhnaf31@gmail.com>
2025-12-25 19:42:45 +0700

    Penambahan dan penyesuaian kode EntrySiswaScreen.kt

--- app/src/main/java/com/example/prak12/view/EntrySiswaScreen.kt
+++ app/src/main/java/com/example/prak12/view/EntrySiswaScreen.kt
@@ -1,142 +1,74 @@
 package com.example.prak12.view
 
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.input.nestedscroll.nestedScroll
-import androidx.compose.ui.res.dimensionResource
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.input.KeyboardType
-import androidx.lifecycle.viewmodel.compose.viewModel
-import com.example.prak12.R
-import com.example.prak12.modeldata.DetailSiswa
-import com.example.prak12.modeldata.UIStateSiswa
-import com.example.prak12.uicontroller.route.DestinasiNavigasi
-import com.example.prak12.viewmodel.EntryViewModel
-import com.example.prak12.viewmodel.provider.PenyediaViewModel
-import kotlinx.coroutines.launch
-
-object DestinasiEntry : DestinasiNavigasi {
-    override val route = "item_entry"
-    override val titleRes = R.string.entry_siswa
-}
-
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun EntrySiswaScreen(
-    navigateBack: () -> Unit,
-    modifier: Modifier = Modifier,
-    viewModel: EntryViewModel = viewModel(factory = PenyediaViewModel.Factory)
-) {
-    val coroutineScope = rememberCoroutineScope()
-    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
-
-    Scaffold(
-        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
-        topBar = {
-            SiswaTopAppBar(
-                title = stringResource(DestinasiEntry.titleRes),
-                canNavigateBack = true,
-                navigateUp = navigateBack,
-                scrollBehavior = scrollBehavior
-            )
-        }
-    ) { innerPadding ->
-        EntrySiswaBody(
-            uiStateSiswa = viewModel.uiStateSiswa,
-            onSiswaValueChange = viewModel::updateUiState,
-            onSaveClick = {
-                coroutineScope.launch {
-                    viewModel.addSiswa()
-                    navigateBack()
-                }
-            },
-            modifier = Modifier
-                .padding(innerPadding)
-                .verticalScroll(rememberScrollState())
-                .fillMaxWidth()
-        )
-    }
-}
+import androidx.compose.ui.unit.dp
+import com.example.prak12.viewmodel.SiswaEvent
+import com.example.prak12.viewmodel.SiswaUIState
 
 @Composable
-fun EntrySiswaBody(
-    uiStateSiswa: UIStateSiswa,
-    onSiswaValueChange: (DetailSiswa) -> Unit,
+fun EntryBody(
+    siswaUIState: SiswaUIState,
+    onSiswaValueChange: (SiswaEvent) -> Unit,
     onSaveClick: () -> Unit,
     modifier: Modifier = Modifier
 ) {
     Column(
-        verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.padding_large)),
-        modifier = modifier.padding(dimensionResource(id = R.dimen.padding_medium))
+        verticalArrangement = Arrangement.spacedBy(16.dp),
+        modifier = modifier.padding(16.dp)
     ) {
-        FormInputSiswa(
-            detailSiswa = uiStateSiswa.detailSiswa,
+        FormInput(
+            siswaEvent = siswaUIState.siswaEvent,
             onValueChange = onSiswaValueChange,
             modifier = Modifier.fillMaxWidth()
         )
         Button(
             onClick = onSaveClick,
-            enabled = uiStateSiswa.isEntryValid,
             shape = MaterialTheme.shapes.small,
             modifier = Modifier.fillMaxWidth()
         ) {
-            Text(stringResource(R.string.btn_submit))
+            Text(text = "Simpan")
         }
     }
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun FormInputSiswa(
-    detailSiswa: DetailSiswa,
+fun FormInput(
+    siswaEvent: SiswaEvent,
     modifier: Modifier = Modifier,
-    onValueChange: (DetailSiswa) -> Unit = {},
+    onValueChange: (SiswaEvent) -> Unit = {},
     enabled: Boolean = true
 ) {
     Column(
         modifier = modifier,
-        verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.padding_medium))
+        verticalArrangement = Arrangement.spacedBy(12.dp)
     ) {
         OutlinedTextField(
-            value = detailSiswa.nama,
-            onValueChange = { onValueChange(detailSiswa.copy(nama = it)) },
-            label = { Text(stringResource(R.string.nama)) },
+            value = siswaEvent.nama,
+            onValueChange = { onValueChange(siswaEvent.copy(nama = it)) },
+            label = { Text("Nama") },
             modifier = Modifier.fillMaxWidth(),
             enabled = enabled,
             singleLine = true
         )
         OutlinedTextField(
-            value = detailSiswa.alamat,
-            onValueChange = { onValueChange(detailSiswa.copy(alamat = it)) },
-            label = { Text(stringResource(R.string.alamat)) },
+            value = siswaEvent.alamat,
+            onValueChange = { onValueChange(siswaEvent.copy(alamat = it)) },
+            label = { Text("Alamat") },
             modifier = Modifier.fillMaxWidth(),
             enabled = enabled,
             singleLine = true
         )
         OutlinedTextField(
-            value = detailSiswa.telpon,
-            onValueChange = { onValueChange(detailSiswa.copy(telpon = it)) },
-            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
-            label = { Text(stringResource(R.string.telpon)) },
+            value = siswaEvent.telpon,
+            onValueChange = { onValueChange(siswaEvent.copy(telpon = it)) },
+            label = { Text("Telepon") },
             modifier = Modifier.fillMaxWidth(),
             enabled = enabled,
             singleLine = true
-        )
-        if (enabled) {
-            Text(
-                text = stringResource(R.string.required_field),
-                modifier = Modifier.padding(start = dimensionResource(id = R.dimen.padding_medium))
-            )
-        }
-        Divider(
-            thickness = dimensionResource(R.dimen.padding_small),
-            modifier = Modifier.padding(bottom = dimensionResource(R.dimen.padding_medium))
         )
     }
 }
